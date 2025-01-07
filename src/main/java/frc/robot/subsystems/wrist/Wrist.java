7deaa12be35a8116cda9720a5c1736dd3ca45b55
BlobbyDude0 <128510114+BlobbyDude0@users.noreply.github.com>
2025-01-06 20:10:37 -0500

    Kent likes booty

--- src/main/java/frc/robot/subsystems/wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/wrist/Wrist.java
@@ -7,32 +7,44 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
   // For instructions on how to implement this class, refer to the README.md file
+  private WristIO m_io;
+  private WristInputsAutoLogged m_inputs;
+  private PIDController m_controller;
+  private double desiredAngle;
 
   public Wrist(WristIO io, PIDController controller) {
     // TODO: Implement the constructor
+    m_io = io;
+    m_controller = controller;
+    m_inputs = new WristInputsAutoLogged();
+    desiredAngle = 0.0;
+    // 3° tollerance
   }
 
   @Override
   public void periodic() {
+    m_io.updateInputs(m_inputs);
+    m_io.setVoltage(m_controller.calculate(m_io.getAngle().getDegrees(), desiredAngle));
     // TODO: Implement this method
   }
 
   public void setDesiredAngle(Rotation2d angle) {
     // TODO: Implement this method
+    desiredAngle = angle.getDegrees();
   }
 
   public Command setDesiredAngleCommand(Rotation2d angle) {
     // TODO: Implement this method
-    return null;
+    return runOnce(() -> setDesiredAngle(angle));
   }
 
   public boolean withinTolerance() {
     // TODO: Implement this method
-    return false;
+    return m_controller.atSetpoint();
   }
 
   public WristInputsAutoLogged getInputs() {
     // TODO: Implement this method
-    return null;
+    return m_inputs;
   }
 }
